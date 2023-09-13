61bc81c7d4e793a216cb04721751dc6eb72604eb
kt <cjtakhar@gmail.com>
2023-09-13 12:18:04 -0700

    add link

--- src/components/donate.jsx
+++ src/components/donate.jsx
@@ -1,11 +1,11 @@
-import {Link} from 'react-router-dom';
+import React from 'react';
 import '../styles/donate.css';
 
 const Donate = () => {
     return (
         <div className="donate-container">
             <h2 className="donate-header">Stand with writers.</h2>
-            <Link className="donate-link" to="/https://entertainmentcommunity.org/">Stand with writers.</Link>
+            <a href="https://www.wgacontract2023.org/take-action/stand-with-writers" className="donate-link">Stand with writers.</a>
         </div>
     );
 }
